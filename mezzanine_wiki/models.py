3b7b4fddf578e49a50b2f7d4f2dafcef2cd56ecd
Dmitry Falk <tfalc@mail.ru>
2012-07-11 15:24:04 +0400

    clean code: remove blank lines

--- mezzanine_wiki/models.py
+++ mezzanine_wiki/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
--- mezzanine_wiki/urls.py
+++ mezzanine_wiki/urls.py
@@ -1,4 +1,3 @@
-
 from django.conf.urls.defaults import patterns, url
 
 
